589487629253653c589cb19fa785e155fe00d145
Your Name <you@example.com>
2021-07-25 15:35:07 +0530

    chatApp

--- chat/src/App.js
+++ chat/src/App.js
@@ -2,22 +2,24 @@ import { ChatEngine} from "react-chat-engine"
 
 import './App.css'
 
-import { ChatFeed } from "./components/ChatFeed"
+import  ChatFeed  from "./components/ChatFeed"
+import LoginForm from "./components/LoginForm"
 
+const projectID="f95e2f3c-7ca2-4477-bc02-69e8c48e3ec9"
 
-
-function App() {
+const App = () => {
+    if (!localStorage.getItem('username')) return <LoginForm />;
+  
     return (
-       <ChatEngine
-       height="100vh"
-       projectID="f95e2f3c-7ca2-4477-bc02-69e8c48e3ec9"
-       userName="Ankita"
-       userSecret="12345678"
-       renderChatFeed={(chatAppProps)=> <ChatFeed  {...chatAppProps}/> }
-            
-       
-       />
-    )
-}
+      <ChatEngine
+        height="100vh"
+        projectID={projectID}
+        userName={localStorage.getItem('username')}
+        userSecret={localStorage.getItem('password')}
+        renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
+        onNewMessage={() => new Audio('https://chat-engine-assets.s3.amazonaws.com/click.mp3').play()}
+      />
+    );
+  };
 
 export default App
